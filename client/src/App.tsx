33a1c0b74000a89842ee1faf859e0c38e307cd2b
Sashwath S <sashwathsub@gmail.com>
2026-03-15 18:30:06 +0530

    ggggg

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,10 +7,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import WhatsAppButton from "@/components/WhatsAppButton";
 
 const queryClient = new QueryClient();
-
 
 const App = () => {
   useEffect(() => {
@@ -48,7 +46,6 @@ const App = () => {
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
-        <WhatsAppButton />
       </TooltipProvider>
     </QueryClientProvider>
   );
